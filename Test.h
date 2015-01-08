3427277754a512d84cc75102557fb1b21014b14b
Aiman Al Gingihy <algingihy@mitre.org>
2015-01-08 13:31:46 -0500

    add test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Al Gingihy, Aiman A on 1/8/15.
+//  Copyright (c) 2015 Al Gingihy, Aiman A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Al Gingihy, Aiman A on 1/8/15.
+//  Copyright (c) 2015 Al Gingihy, Aiman A. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
